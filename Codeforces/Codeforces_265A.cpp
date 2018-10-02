3fb6948a7c1e2627617eae81a52f9cfc4940b818
Naimul Haque <im.tumul@gmail.com>
2018-10-02 20:35:25 +0600

    Colorful Stones (Simplified Edition)

---
+++ Codeforces/Codeforces_265A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str, instructions;
+    cin >> str >> instructions;
+
+    int current_pos = 0;
+    // if the instruction matches the charecter at current_pos of str, go to the next position
+    for (char ch: instructions) {
+        if (str.at(current_pos) == ch) 
+            current_pos++;
+    }
+
+    // print the current position in 1 based index
+    cout << current_pos + 1 << endl;
+    return 0;
+}
