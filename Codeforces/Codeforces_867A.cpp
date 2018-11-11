a678f3f225735014f39e9a8942797c379c55bfdd
Naimul Haque <im.tumul@gmail.com>
2018-11-11 18:34:44 +0600

    Between the Offices

---
+++ Codeforces/Codeforces_867A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main () {
+    int n, f_to_s = 0, s_to_f = 0;
+    string s;
+    cin >> n >> s;
+
+    for (int i = 0; i < (int) s.length() - 1; i++) {
+        if (s[i] == s[i + 1])
+            continue;
+
+        if (s[i] == 'F' && s[i + 1] == 'S')
+            f_to_s++;
+
+        else if (s[i] == 'S' && s[i + 1] == 'F')
+            s_to_f++;   
+    }
+
+    if (s_to_f > f_to_s)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
